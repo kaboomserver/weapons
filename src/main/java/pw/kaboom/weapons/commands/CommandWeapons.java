3b62022f1bffd122c06493dccaece5b62740597b
mathiascode <mail@mathias.is>
2019-09-22 05:40:59 +0300

    Import ConsoleCommandSender

--- src/main/java/pw/kaboom/weapons/commands/CommandWeapons.java
+++ src/main/java/pw/kaboom/weapons/commands/CommandWeapons.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 
 import org.bukkit.entity.Player;
 
